64e5813e7cc565cd1227170fb913c930005f9db2
dustisme <androsrivas@outlook.com>
2025-01-29 19:09:02 +0100

    refactor use product hook. create use category hook

--- src/hooks/useProducts.jsx
+++ src/hooks/useProducts.jsx
@@ -3,53 +3,84 @@ import { createProducts, getAllProducts, updateProduct, deleteProduct } from "..
 
 const useProduct = () => {
   const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loadingCount, setLoadingCount] = useState(0);
+  const [errors, setErrors] = useState([]);
+
+  const setLoading = (isLoading) => {
+    setLoadingCount((prev) => prev + (isLoading ? 1 : -1));
+  }
 
   const getAllProductsFromApiService = async () => {
+    setLoading(true);
+    setErrors([]);
     try {
       const data = await getAllProducts();
       setProducts(data);
-    } catch (error) {
-      console.error("Error fetching products:", error);
+    } catch (errors) {
+      setErrors((prevErrors) => [...prevErrors, "Error fetching products."]);
+      console.error("Error fetching products: ", errors);
     } finally {
       setLoading(false);
     }
   };
 
   const createProduct = async (newProduct) => {
+    setLoading(true);
+    setErrors([]);
     try {
       const createdProduct = await createProducts(newProduct);
       setProducts((prevProducts) => [...prevProducts, createdProduct]);
     } catch (error) {
+      setErrors((prevErrors) => [...prevErrors, "Error creating products."]);
       console.error("Error creating product:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
   const updateProductById = async (id, updatedProduct) => {
+    setLoading(true);
+    setErrors([]);
     try {
       const updated = await updateProduct(id, updatedProduct);
       setProducts((prevProducts) =>
         prevProducts.map((product) => (product.id === id ? updated : product))
       );
     } catch (error) {
+      setErrors((prevErrors) => [...prevErrors, "Error updating products."]);
       console.error("Error updating product:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
   const deleteProductById = async (id) => {
+    setLoading(true);
+    setErrors([]);
     try {
       await deleteProduct(id);
       setProducts((prevProducts) => prevProducts.filter((product) => product.id !== id));
     } catch (error) {
+      setErrors((prevErrors) => [...prevErrors, "Error deleting products."]);
       console.error("Error deleting product:", error);
+    } finally {
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     getAllProductsFromApiService();
-  }, []);
+  }, [getAllProductsFromApiService]);
 
-  return { products, loading, createProduct, updateProductById, deleteProductById };
+  return { 
+    products, 
+    loading: loadingCount > 0, 
+    errors,
+    createProduct, 
+    updateProductById, 
+    deleteProductById,
+    refreshProducts: getAllProductsFromApiService 
+  };
 };
 
 export default useProduct;
